5f4b3b2d86d53334176ccc16be566842f44e509c
crowjambo <evaldascrow@gmail.com>
2020-05-20 04:49:44 +0300

    init

---
+++ wordpressAPI_app/wordpressAPI_app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wordpressAPI_app
+//
+//  Created by Evaldas Paulauskas on 2020-05-20.
+//  Copyright © 2020 Evaldas Paulauskas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
